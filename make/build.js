970d1f1a73f71e80e42b449e891eac13f04e4d2d
Dustin Diaz <polvero@gmail.com>
2011-09-26 16:58:18 -0700

    - new flags for mobile with android and iPhone - bump minor version

--- make/build.js
+++ make/build.js
@@ -1,24 +1,25 @@
 require('smoosh').config({
   "JAVASCRIPT": {
-    "DIST_DIR": "./",
-    "bowser": [
-      "./src/copyright.js",
-      "./src/bowser.js"
+    "DIST_DIR": "./"
+  , "bowser": [
+        "./src/copyright.js"
+      , "./src/bowser.js"
     ]
-  },
-  "JSHINT_OPTS": {
-    "boss": true,
-    "forin": false,
-    "curly": true,
-    "debug": false,
-    "devel": false,
-    "evil": false,
-    "regexp": false,
-    "undef": false,
-    "sub": false,
-    "white": true,
-    "indent": 2,
-    "whitespace": true,
-    "asi": false
   }
-}).run().build().analyze();
+  , "JSHINT_OPTS": {
+      "boss": true
+    , "forin": false
+    , "curly": false
+    , "debug": false
+    , "devel": false
+    , "evil": false
+    , "regexp": false
+    , "undef": false
+    , "sub": true
+    , "white": false
+    , "indent": 2
+    , "whitespace": true
+    , "asi": true
+    , "laxbreak": true
+  }
+}).run().build().analyze()
--- src/bowser.js
+++ src/bowser.js
@@ -1,10 +1,13 @@
 /*!
   * Bowser - a browser detector
-  * copyright Dustin Diaz 2011
   * https://github.com/ded/bowser
-  * MIT License
+  * MIT License | (c) Dustin Diaz 2011
   */
-!function (context) {
+!function (name, definition) {
+  if (typeof define == 'function') define(definition)
+  else if (typeof module != 'undefined' && module.exports) module.exports['browser'] = definition()
+  else this[name] = definition()
+}('bowser', function () {
   /**
     * navigator.userAgent =>
     * Chrome:  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.57 Safari/534.24"
@@ -12,58 +15,66 @@
     * Safari:  "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1"
     * IE:      "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C)"
     * Firefox: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0) Gecko/20100101 Firefox/4.0"
+    * iPhone:  "Mozilla/5.0 (iPhone Simulator; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5"
+    * Android: "Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; T-Mobile G2 Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
     */
 
-  var ua = navigator.userAgent,
-      ie = /msie/i.test(ua),
-      chrome = /chrome/i.test(ua),
-      safari = /safari/i.test(ua) && !chrome,
-      opera = /opera/i.test(ua),
-      firefox = /firefox/i.test(ua),
-      gecko = /gecko\//i.test(ua);
+  var ua = navigator.userAgent
+    , t = true
+    , ie = /msie/i.test(ua)
+    , chrome = /chrome/i.test(ua)
+    , safari = /safari/i.test(ua) && !chrome
+    , iphone = /iphone/i.test(ua)
+    , android = /android/i.test(ua)
+    , opera = /opera/i.test(ua)
+    , firefox = /firefox/i.test(ua)
+    , gecko = /gecko\//i.test(ua)
+    , webkitVersion = /version\/(\d+(\.\d+)?)/i
 
   function detect() {
 
-    if (ie) {
-      return {
-        msie: 1,
-        version: ua.match(/msie (\d+(\.\d+)?);/i)[1]
-      };
+    if (ie) return {
+        msie: t
+      , version: ua.match(/msie (\d+(\.\d+)?);/i)[1]
     }
-    if (chrome) {
-      return {
-        webkit: 1,
-        chrome: 1,
-        version: ua.match(/chrome\/(\d+(\.\d+)?)/i)[1]
-      };
+    if (chrome) return {
+        webkit: t
+      , chrome: t
+      , version: ua.match(/chrome\/(\d+(\.\d+)?)/i)[1]
     }
-    if (safari) {
-      return {
-        webkit: 1,
-        safari: 1,
-        version: ua.match(/version\/(\d+(\.\d+)?)/i)[1]
-      };
+    if (iphone) return {
+        webkit: t
+      , iphone: t
+      , mobile: t
+      , version: ua.match(webkitVersion)[1]
     }
-    if (opera) {
-      return {
-        opera: 1,
-        version: ua.match(/version\/(\d+(\.\d+)?)/i)[1]
-      };
+    if (android) return {
+        webkit: t
+      , android: t
+      , mobile: t
+      , version: ua.match(webkitVersion)[1]
+    }
+    if (safari) return {
+        webkit: t
+      , safari: t
+      , version: ua.match(webkitVersion)[1]
+    }
+    if (opera) return {
+        opera: t
+      , version: ua.match(webkitVersion)[1]
     }
     if (gecko) {
       var o = {
-        gecko: 1,
-        version: ua.match(/firefox\/(\d+(\.\d+)?)/i)[1]
-      };
-      if (firefox) {
-        o.firefox = 1;
+          gecko: t
+        , version: ua.match(/firefox\/(\d+(\.\d+)?)/i)[1]
       }
-      return o;
+      if (firefox) o.firefox = t
+      return o
     }
 
   }
 
-  var bowser = detect();
+  var bowser = detect()
 
   // Graded Browser Support
   // http://developer.yahoo.com/yui/articles/gbs
@@ -72,7 +83,7 @@
       (bowser.firefox && bowser.version >= 3.6) ||
       (bowser.safari && bowser.version >= 5) ||
       (bowser.opera && bowser.version >= 9.5)) {
-    bowser.a = true;
+    bowser.a = t;
   }
 
   else if ((bowser.msie && bowser.version < 6) ||
@@ -80,13 +91,8 @@
       (bowser.firefox && bowser.version < 3.6) ||
       (bowser.safari && bowser.version < 5) ||
       (bowser.opera && bowser.version < 9.5)) {
-    bowser.c = true;
-  } else {
-    bowser.x = true;
-  }
+    bowser.c = t
+  } else bowser.x = t
 
-  typeof module !== 'undefined' && module.exports ?
-    (module.exports.browser = bowser) :
-    (context.bowser = bowser);
-
-}(this);
+  return bowser
+})

efe1c6719a75b05e5372496d9fe8670125364001
Dustin Diaz <polvero@gmail.com>
2014-01-27 14:01:01 -0800

    closes #16 - move commonjs to first check

--- bowser.js
+++ bowser.js
@@ -5,8 +5,8 @@
   */
 
 !function (name, definition) {
-  if (typeof define == 'function') define(definition)
-  else if (typeof module != 'undefined' && module.exports) module.exports['browser'] = definition()
+  if (typeof module != 'undefined' && module.exports) module.exports['browser'] = definition()
+  else if (typeof define == 'function') define(definition)
   else this[name] = definition()
 }('bowser', function () {
   /**

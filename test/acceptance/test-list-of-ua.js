d3fec85440fdfb6beee566051f3dcd791ba6f3ad
Denis Demchenko <lancedikson@gmail.com>
2017-10-17 21:55:45 +0300

    Fix integration tests
    Provided more details for the testing thing

--- test/acceptance/test-list-of-ua.js
+++ test/acceptance/test-list-of-ua.js
@@ -5,12 +5,12 @@ import Bowser from '../../src/new-bowser';
 
 const listOfUA = yaml.load(path.join(__dirname, 'useragentstrings.yml'));
 
-test('Check all the test browsers', t => {
-  for (const browserName in listOfUA) {
-    listOfUA[browserName].forEach((browser) => {
+for (const browserName in listOfUA) {
+  listOfUA[browserName].forEach((browser) => {
+    test('Check all the test browsers', t => {
       const parsed = new Bowser(browser.ua).parse().getResult();
       t.deepEqual(parsed, browser.spec, `${browser.ua}`);
       t.is(parsed.browser.name, browserName, `${browser.ua}`);
     });
-  }
-});
+  });
+}

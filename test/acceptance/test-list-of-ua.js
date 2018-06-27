6f3b13f9fd8084aca1ce9f1c08f00b138111cae5
Denis Demchenko <lancedikson@gmail.com>
2018-06-27 22:52:43 +0300

    Replace the main bowser file

--- test/acceptance/test-list-of-ua.js
+++ test/acceptance/test-list-of-ua.js
@@ -1,13 +1,13 @@
 import test from 'ava';
 import yaml from 'yamljs';
 import path from 'path';
-import Bowser from '../../src/new-bowser';
+import Bowser from '../../src/bowser';
 
 const listOfUA = yaml.load(path.join(__dirname, 'useragentstrings.yml'));
 
 for (const browserName in listOfUA) {
   listOfUA[browserName].forEach((browser) => {
-    test('Check all the test browsers', t => {
+    test('Check all the test browsers', (t) => {
       const parsed = new Bowser(browser.ua).parse().getResult();
       t.deepEqual(parsed, browser.spec, `${browser.ua}`);
       t.is(parsed.browser.name, browserName, `${browser.ua}`);

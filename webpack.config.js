8e9a6c7b4afdff23cb1cbdad842fc6a25e22a011
Denis Demchenko <lancedikson@gmail.com>
2018-07-22 19:31:28 +0300

    Compile 2 versions of files (es5 and bundled)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
   mode: 'production', // "production" | "development" | "none"
   // Chosen mode tells webpack to use its built-in optimizations accordingly.
-  entry: ['babel-polyfill', './src/bowser.js'], // string | object | array
+  entry: {
+    bundled: ['babel-polyfill', './src/bowser.js'],
+    es5: './src/bowser.js',
+  }, // string | object | array
   // defaults to ./src
   // Here the application starts executing
   // and webpack starts bundling
@@ -12,7 +15,7 @@ module.exports = {
     path: path.resolve(__dirname, './'), // string
     // the target directory for all output files
     // must be an absolute path (use the Node.js path module)
-    filename: 'compiled.js', // string
+    filename: '[name].js', // string
     // the filename template for entry chunks
     library: 'bowser',
     libraryTarget: 'umd', // universal module definition

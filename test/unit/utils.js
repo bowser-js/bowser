cad365b53e3bc9172898b4f33a67cc438dca1180
Oliver foster <oliver.foster@kineo.com>
2019-12-19 11:38:49 +0000

    Fixed getSecondMatch test

--- test/unit/utils.js
+++ test/unit/utils.js
@@ -21,7 +21,7 @@ test('getFirstMatch', (t) => {
 });
 
 test('getSecondMatch', (t) => {
-  const matchedVersion = getSecondMatch(/version\/(\S+)/ig, 'Chrome Version/11.11.11 Chrome Version/22.22.22');
+  const matchedVersion = getSecondMatch(/version\/(\S+).*version\/(\S+)/i, 'Chrome Version/11.11.11 Chrome Version/22.22.22');
   t.is(matchedVersion, '22.22.22');
 });
 

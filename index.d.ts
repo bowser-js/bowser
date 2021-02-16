efbc4ea94fa8e281b8ad60e8c4560f5edf1d2804
James Cleveland <jc@blit.cc>
2021-02-16 22:44:27 +0000

    Correct types
    Bowser doesn't actually export methods as named exports, they are only destructured from the `Bowser` class. The types should reflect this.

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 // Project: https://github.com/lancedikson/bowser
 // Definitions by: Alexander P. Cerutti <https://github.com/alexandercerutti>,
 
-export = Bowser;
+export default Bowser;
 export as namespace Bowser;
 
 declare namespace Bowser {
